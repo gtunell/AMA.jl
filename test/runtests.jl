d16b04faab94848febad78f2979b2af44ed66d25
Gary Anderson <asymptoticallystable@gmail.com>
2018-06-04 18:33:39 -0400

    starting a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,19 @@
 using AMA
 using Base.Test
-
-# write your own tests here
-@test 2 == 2
+using MAT
+file=matopen("./matFiles/hmat.mat")
+originalH=read(file,"h")
+close(file)
+file=matopen("./matFiles/exactRes.mat")
+exactShiftH=read(file,"h")
+exactShiftQ=read(file,"q")
+close(file)
+neq=4::Int64
+qRows=4::Int64
+qCols=8::Int64
+#=
+(newH,qq,iq,nExact)= exactShift(originalH,qq,iq,qRows,qCols,neq)
+@test nExact==2
+@test isapprox(newH,exactShiftH,rtol::Real=0.1e-16,atol::Real=0)
+=#
+@test  2==2
