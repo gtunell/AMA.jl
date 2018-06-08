e2fc79b87ea38344bcc76e3d70e8ab05bc73fc9d
Gregory Tunell <m1gmt00@node007b.rsma.frb.gov>
2018-06-08 16:02:05 -0400

    exactShift skeleton

---
+++ src/exactShift.jl
@@ -0,0 +1,13 @@
+"""
+    exactShift(h, q, iq, qrows, qcols, neq)
+
+Compute the exact shiftrights and store them in q.
+"""
+function exactShift(h::Array{Float64,2}, q::Array{Float64,2},
+									iq::Int64, qrows:Int64, 
+									qcols:Int64, neq::Int64) 
+
+
+
+
+end # exactShift
