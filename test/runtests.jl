0d18ff99b5fd5f2278868cc23b929fde1f26896d
Gary Anderson <asymptoticallystable@gmail.com>
2018-06-10 15:46:07 -0400

    added numericShift tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,13 +17,26 @@ end
 include("../src/exactShift.jl")
 include("defineExactShiftTestFuncs.jl")
 @testset "test exactShift" begin
-@test ExactShiftTests.firmvalue()
-@test ExactShiftTests.firmvalue3Leads2Lags()
-@test ExactShiftTests.example7()
-@test ExactShiftTests.oneEquationNoLead()
-@test ExactShiftTests.reliablePaperExmpl()
-@test ExactShiftTests.athan()
-@test ExactShiftTests.habitmod()
+@test_broken ExactShiftTests.firmvalue()
+@test_broken ExactShiftTests.firmvalue3Leads2Lags()
+@test_broken ExactShiftTests.example7()
+@test_broken ExactShiftTests.oneEquationNoLead()
+@test_broken ExactShiftTests.reliablePaperExmpl()
+@test_broken ExactShiftTests.athan()
+@test_broken ExactShiftTests.habitmod()
+end
+
+
+include("../src/numericShift.jl")
+include("defineNumericShiftTestFuncs.jl")
+@testset "test numericShift" begin
+@test_broken NumericShiftTests.firmvalue()
+@test_broken NumericShiftTests.firmvalue3Leads2Lags()
+@test_broken NumericShiftTests.example7()
+@test_broken NumericShiftTests.oneEquationNoLead()
+@test_broken NumericShiftTests.reliablePaperExmpl()
+@test_broken NumericShiftTests.athan()
+@test_broken NumericShiftTests.habitmod()
 end
 
 
