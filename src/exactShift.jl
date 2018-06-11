dde958cc8f4369f07c71519a926762cc3e9990f7
Gregory Tunell <m1gmt00@node007b.rsma.frb.gov>
2018-06-11 17:32:24 -0400

    fixed logical errors

--- src/exactShift.jl
+++ src/exactShift.jl
@@ -1,36 +1,37 @@
 module AMAFUNCS
 
+include("shiftRight.jl")
+
 """
     exactShift(h, q, iq, qrows, qcols, neq)
 
 Compute the exact shiftrights and store them in q.
 """
-function exactShift(hh::Array{Float64,2}, qq::Array{Float64,2}, iq::Int64, qRows::Int64, qCols::Int64, neq::Int64) 
+function exactShift!(hh::Array{Float64,2}, qq::Array{Float64,2}, iq::Int64, qRows::Int64, qCols::Int64, neq::Int64) 
 
     nexact = 0
-    left = hh[:, 1:qCols]
-    right = hh[:, (qCols + 1):(qCols + neq)]
+    left = 1:qCols
+    right = (qCols + 1):(qCols + neq)
     
-    zerorows = hh[:, size(right, 2)]'
+    zerorows = hh[:, right]'
     zerorows = abs.(zerorows)
-    zerorows = sum(zerorows)
+    zerorows = sum(zerorows, 1)
     zerorows = find(zerorows)
 
-    Base.display(zerorows)
-    
-    while (any(zerorows)) # && (iq <= qRows))
-        nz = length(zerorows)
-        qq[(iq + 1):(iq + nz)] = hh[zerorows, left]
-        hh[zerorows,:] = shiftRight(hh[zerorows, :], neq)
+    while any(rowSum->(rowSum != 0), zerorows) && (iq <= qRows)
+        nz = size(zerorows, 1)
+        qq[(iq + 1):(iq + nz), :] = hh[zerorows, left]
+        hh[zerorows,:] = AMAFUNCS.shiftRight!(hh[zerorows, :], neq)
         iq = iq + nz
 
-        zerorows = hh[:, size(right, 2)]'
-        zerorows = abs(zerorows)
+        zerorows = hh[:, right]'
+        zerorows = abs.(zerorows)
         zerorows = sum(zerorows)
         zerorows = find(zerorows)
     end # while
 
+    return qq
+    
 end # exactShift
 
 end # module
-
