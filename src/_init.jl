4c029958958ade5877ed4373699f0c8220c426e5
why <asymptoticallystable@gmail.com>
2018-06-13 09:33:40 -0400

    add eig tests

--- src/_init.jl
+++ src/_init.jl
@@ -1,5 +1,5 @@
 println("running AMA.jl/src/_init.jl")
-theFile="/msu/scratch2/m1gsa00/git/AMA.jl/src/_init.jl"
+theFile="/Users/garyanderson/git/AMA.jl/src/_init.jl"
 if length(findin(theFile,LOAD_PATH))==0
     push!(LOAD_PATH,pwd())
     println("pushing ",theFile," onto LOAD_PATH")
