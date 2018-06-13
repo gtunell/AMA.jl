374496b7f976c78504fd8873400b43c8c8c43cad
Gary Anderson <asymptoticallystable@gmail.com>
2018-06-13 15:59:37 -0400

    add eigsystest

---
+++ src/eigenSys!.jl
@@ -0,0 +1,12 @@
+"""
+    eigenSys!(h, qcols, neq)
+
+Compute the numeric shiftrights and store them in q.
+"""
+function eigenSys!(aa::Array{Float64,2}, uprbnd::Float64, rowsLeft::Int64) 
+
+(10,20,30)
+
+
+end # buildA
+
