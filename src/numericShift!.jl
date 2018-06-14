ad43cb7198652e382f4bba721d086c39b1361547
Gregory Tunell <m1gmt00@node007b.rsma.frb.gov>
2018-06-14 11:28:21 -0400

    developing numericShift

--- src/numericShift!.jl
+++ src/numericShift!.jl
@@ -1,11 +1,21 @@
 """
-    numericShift(h, q, iq, qrows, qcols, neq)
+    numericShift(hh, qq, iq, qrows, qcols, neq, condn)
 
 Compute the numeric shiftrights and store them in q.
 """
-function numericShift!(h::Array{Float64,2}, q::Array{Float64,2}, iq::Int64, qrows::Int64, qcols::Int64, neq::Int64) 
+function numericShift!(hh::Array{Float64,2}, qq::Array{Float64,2}, iq::Int64, qrows::Int64, qcols::Int64, neq::Int64, condn::Float64) 
 
-(1,20,3,5)
+    nnumeric = 0
+    left = 1:qcols
+    right = qcols + 1:qcols + neq
+
+    (Q, R, E) = qr( hh(:, right) )
+    zerorows = abs.(diagm(R))
+    zerorows = find(x->(x <= 10^-10), zerorows)
+
+    display(zerorows)
+
+    
 
 
 end # numericShift
