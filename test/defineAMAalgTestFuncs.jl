c4d1f895e31a59a6a6e9f548ca175c290dfd4909
Gregory Tunell <m1gmt00@node009d.rsma.frb.gov>
2018-06-25 16:55:39 -0400

    AMA change

--- test/defineAMAalgTestFuncs.jl
+++ test/defineAMAalgTestFuncs.jl
@@ -13,9 +13,9 @@ qRows=(neq*nlead)::Int64;qCols=(neq*(nlag+nlead))::Int64
 
 
 bb=[0.  0.228571  0.  0.;
-0.  0.4  0.  0.;
-0.  0.  0.  0.;
-0.  0.  0.  1.]::Array{Float64,2}
+ 0.  0.4  0.  0.;
+ 0.  0.  0.  0.;
+ 0.  0.  0.  1.]::Array{Float64,2}
 
 rts=[1.1;
 1.;
