60dcc4f101fcfa6df4f4170e93bce1470c275430
Gregory Tunell <m1gmt00@node004c.rsma.frb.gov>
2018-07-10 17:20:33 -0400

    c to julia sparseAMA fixed conversions

--- cSparseAMA/cSparseAMA.jl
+++ cSparseAMA/cSparseAMA.jl
@@ -1,15 +1,54 @@
-function cSparseAMA(maxNumberOfHElements, discreteTime, hrows, hcols, leads,
-         hmat, hmatj, hmati, newHmat, newHmatj, newHmati,
-         auxiliaryInitialConditions, rowsInQ, qmat, qmatj, qmati,
-         essential, rootr, rooti, returnCode, aPointerToVoid)
+push!(Libdl.DL_LOAD_PATH,
+      "/msu/home/m1gmt00/summer_project/AMA.jl/cSparseAMA/src")
+
+function cSparseAMA( hh, nlags, nleads )
+
+    neq = size(hh, 1)
+    qmax = length(hh)
+    qcols = neq*(nlags+nleads)
     
+    maxNumberOfHElements = length(hh)
+    discreteTime = 1
+    hrows = size(hh, 1)
+    hcols = size(hh, 2)
+
+    (I, J, V) = findnz(hh)
+    hmat = V
+    hmatj = J
+    hmati = I
+
+    newHmat = zeros(qmax)
+    newHmatj = zeros(qmax)
+    newHmati = zeros(hrows + 1)
+    
+    auxiliaryInitialConditions = 0
+    rowsInQ = 0
+    
+    qmat = zeros(qmax)
+    qmatj = zeros(qmax)
+    qmati = zeros(hrows*(nleads+nlags+1)+1)
+    
+    rootr = zeros(qcols)
+    rooti = zeros(qcols)
+    essential = qcols
+    
+    returnCode = 0
+    aPointerToVoid = nothing
+
     ccall((:sparseAim, "libSPARSEAMA"), Void,
-         (Ref{Int32}, Int32, Int32, Int32, Int32,
-          Ref{Float64}, Ref{Int32}, Ref{Int32}, Ref{Float64}, Ref{Int32}, Ref{Int32},
-          Ref{Int32}, Ref{Int32}, Ref{Float64}, Ref{Int32}, Ref{Int32},
-          Ref{Int32}, Ref{Float64}, Ref{Float64}, Ref{Int32}, Ref{Void}),
-         maxNumberOfHElements, discreteTime, hrows, hcols, leads,
-         hmat, hmatj, hmati, newHmat, newHmatj, newHmati,
-         auxiliaryInitialConditions, rowsInQ, qmat, qmatj, qmati,
-         essential, rootr, rooti, returnCode, aPointerToVoid)
+         (  Ptr{Int32}, Int32,
+            Int32, Int32, Int32,
+            Ptr{Float64}, Ptr{Int32}, Ptr{Int32}, # points to mem of first ele
+            Ptr{Float64}, Ptr{Int32}, Ptr{Int32}, # points to mem of first ele
+            Ptr{Int32}, Ptr{Int32},
+            Ptr{Float64}, Ptr{Int32}, Ptr{Int32},
+            Ptr{Int32}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr   ),
+         Ptr{Int32}(maxNumberOfHElements), Int32(discreteTime),
+         Int32(hrows), Int32(hcols), Int32(nleads),
+         Float64.(hmat), Int32.(hmatj), Int32.(hmati), 
+         Float64.(newHmat), Int32.(newHmatj), Int32.(newHmati), 
+         Ptr{Int32}(auxiliaryInitialConditions), Ptr{Int32}(rowsInQ),
+         Float64.(qmat), Int32.(qmatj), Int32.(qmati),
+         Int32(essential), Float64.(rootr), Float64.(rooti),
+         Int32(returnCode), aPointerToVoid)
 end
