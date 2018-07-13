b2ba080d365d8abfa73f531867723a4c0d06b342
Gregory Tunell <m1gmt00@node004c.rsma.frb.gov>
2018-07-13 14:25:58 -0400

    switching ccall to callSparseAim.c

---
+++ cSparseAMA/.#testCSparseAMA.jl
@@ -0,0 +1,1 @@
+m1gmt00@node004c.rsma.frb.gov.58631:1530055045
--- test/defineAMAalgTestFuncs.jl
+++ test/defineAMAalgTestFuncs.jl
@@ -1,9 +1,8 @@
 module AMAalgTests
 
- include("../src/AMA.jl")
+# include("../src/AMA.jl")
 # test AMAalg
-using .AMA, MAT
-
+using ..AMA, MAT
     
 #tweaked= False
 # test AMAalg firmvalueFalse example
@@ -29,9 +28,6 @@ hh=read(file,"hh")
 hh=if(typeof(hh)==(Array{Float64,2})) hh else hcat(hh) end
 close(file)
 
-    
-
-    
 ia=3::Int64
 
 nex=3::Int64
