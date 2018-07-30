9815376534235e98a1344d5427d970b239ff14c6
Gregory Tunell <m1gmt00@node004c.rsma.frb.gov>
2018-07-30 11:49:26 -0400

    script to make library

--- src/AMA.jl
+++ src/AMA.jl
@@ -4,7 +4,7 @@ module AMA
 # http://www.stochasticlifestyle.com/finalizing-julia-package-documentation-testing-coverage-publishing/
 
 # Set-up for callSparseAim
-const lib = Libdl.dlopen(joinpath(dirname(@__FILE__), "..", "deps", "libSPARSEAMA"))
+const lib = Libdl.dlopen(realpath(joinpath(dirname(@__FILE__), "..", "deps", "libSPARSEAMA")))
 sym = Libdl.dlsym(lib, :callSparseAim)
 
 # Include all files    
@@ -13,23 +13,6 @@ for (root, dirs, files) in walkdir(joinpath(dirname(@__FILE__), "AMA"))
         include(joinpath("AMA", file))
     end
 end
-
-#=
-# Include relevant files
-include("exactShift!.jl")
-include("numericShift!.jl")
-include("shiftRight!.jl")
-include("buildA!.jl")
-include("augmentQ!.jl")
-include("eigenSys!.jl")
-include("reducedForm.jl")
-include("AMAalg.jl")
-include("checkSpanning.jl")
-include("deleteCols.jl")
-include("deleteRows.jl")
-include("callSparseAim.jl")
-include("checkAMA.jl")
-=#
 
 # Export all functions
 export exactShift!, numericShift!, shiftRight!, buildA!, augmentQ!, eigenSys!, reducedForm, AMAalg, sameSpan, deleteCols, deleteRows, callSparseAim, checkAMA
--- src/AMA/callSparseAim.jl
+++ src/AMA/callSparseAim.jl
@@ -18,13 +18,8 @@ function callSparseAim( hh, leads, lags )
     # use the library libSPARSEAMA to call c function ...
     # libSPARSEAMA is a shared library that combines sparseAMA
     # and LAPACK. LAPACK must be compiled with -fPIC.
-    #Libdl.push!(Libdl.DL_LOAD_PATH, ".")
-    #Libdl.push!(Libdl.DL_LOAD_PATH, "./deps")
-    #Libdl.push!(Libdl.DL_LOAD_PATH, "../deps")
-    #lib = Libdl.dlopen(pwd() * "/../deps/libSPARSEAMA")
- 
-    #sym = Libdl.dlsym(lib, :callSparseAim)
-    #include("../deps/build.jl")
+    # lib = Libdl.dlopen(pwd() * "/../deps/libSPARSEAMA")
+    # sym = Libdl.dlsym(lib, :callSparseAim)
     ccall(sym, Void,
           (  Ptr{Float64}, Int32, Int32, Int32, Int32,
           Int32, Int32, Int32,
