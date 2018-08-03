cba67a082ce30edd6b1cf3876015eda1a9ee4e3d
Gregory Tunell <m1gmt00@node004c.rsma.frb.gov>
2018-08-03 12:49:20 -0400

    install gfortran mac

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,10 @@
-#cd(dirname(@__FILE__))
-#run(`make`)
+cd(dirname(@__FILE__))
+run(`make`)
 
-#if is_apple()
-#    run(`brew tap homebrew/science`)
-#    run(`brew instal hdf5`)
-#    Pkg.add("HDF5")
-#end
+if is_apple()
+    run(`brew tap homebrew/science`)
+    run(`brew instal hdf5`)
+    Pkg.add("HDF5")
+end
 
 Pkg.add("MAT")
